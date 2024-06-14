1599f6651a1e4b29de1b1c5e40333972b4f25f24
YordanTsonev <145608256+YordanTsonev@users.noreply.github.com>
2024-06-14 19:52:19 +0300

    Update enemy.py

--- code/enemy.py
+++ code/enemy.py
@@ -1,18 +1,17 @@
 import pygame
 
-
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, platform):
         super().__init__()
         self.image = pygame.transform.scale(
             pygame.image.load('../enemy/enemy_1.png').convert_alpha(), (100, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.platform = platform
+        self.rect.centerx = platform.rect.centerx
+        self.rect.bottom = platform.rect.top
         self.direction = 1
-        self.on_ground = False
 
     def update(self):
         self.rect.x += self.direction
-        if self.rect.right > 800 or self.rect.left < 0:
+        if self.rect.right >= self.platform.rect.right or self.rect.left <= self.platform.rect.left:
             self.direction *= -1
