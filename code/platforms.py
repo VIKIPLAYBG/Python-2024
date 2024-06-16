b75538e73743346d40b68e97421ae2bb2f4f39a7
YordanTsonev <145608256+YordanTsonev@users.noreply.github.com>
2024-06-16 09:18:43 +0300

    max_enemies-3

--- code/platforms.py
+++ code/platforms.py
@@ -1,8 +1,6 @@
 import pygame
+import pygame
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, platform_type):
@@ -14,3 +12,10 @@ class Platform(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
         self.rect.x = x
         self.rect.y = y
+
+        self.max_enemies = 3
+        self.enemy_sprites = pygame.sprite.Group()
+
+    def can_spawn_enemy(self):
+       
+        return len(self.enemy_sprites) < self.max_enemies
